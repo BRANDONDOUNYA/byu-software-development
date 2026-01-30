f60edd2eee60a2a4a01fa88f21792191464cd920
ASILATSA DOUNYA BRANDON <bdounya01@gmail.com>
2026-01-30 23:10:37 +0100

    Add files via upload

---
+++ w01-clever-stories/project1_CleverStories.py
@@ -0,0 +1,24 @@
+"""
+Clever Stories project
+Author: ASILATSA DOUNYA BRANDON
+"""
+#add blank lines
+print()
+print(f"Please enter the following:")
+print()
+
+#ask user inputs
+adjective = input("adjectif: ")
+animal = input("animal: ")
+verb1 = input("verb: ")
+exclamation = input("exclamation: ")
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+print("\n")
+
+#print output
+print(f"Your story is:")
+print("\n")
+print(f'The other day, I was really in trouble. It all started when I saw a very {adjective.lower()} {animal.lower()} {verb1.lower()} down the hallway. "{exclamation.capitalize()}!" I yelled. But all I could think to do was to {verb2.lower()} over and over. Miraculously, that caused it to stop, but not before it tried to {verb3.lower()} right in front of my family.')
+print("\n")
+
